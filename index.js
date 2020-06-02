2c647428a0b4bc5eec00da4ba4f876b9e96fabaa
hectorconde90 <hecopa50@gmail.com>
2020-06-02 13:58:20 +0200

    actualizacion dom

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const inputName = document.getElementById('name');
+const inputPassword = document.getElementById('pass');
+const enviar = document.getElementById('enviar');
+
+const takeUsers = () => {
+    const user = {
+        name: inputName.value,
+        password: inputPassword.value
+    }
+    // console.log(user)
+
+}
 const restPhoneApi = axios.create({
-    baseURL: 'https://desolate-reef-58953.herokuapp.com/api/persons'
+    baseURL: 'https://desolate-reef-58953.herokuapp.com/'
 });
+
+
+
 
 const getAllPhone = () => {
     restPhoneApi
-        .get()
+        .get('api/persons')
         .then(responseFromApi => {
             console.log(responseFromApi.data);
             document.write(JSON.stringify(responseFromApi.data))
@@ -12,6 +27,29 @@ const getAllPhone = () => {
         .catch(err => console.log('error ', err));
 }
 
+const hacerUnPost = () => {
+    const user = {
+        user: inputName.value,
+        password: inputPassword.value
+    }
+    console.log(user);
+    restPhoneApi
+        .post('login', user)
+        .then(res => console.log('login succsesful'))
+        .catch(err => console.log(err));
+    
+}
+
+const hacerUnLogout = () => {
+    restPhoneApi
+        .get('logout')
+        .then(responseFromApi => {
+            console.log(responseFromApi.data);
+        })
+        .catch(err => console.log('error ', err));
+}
 
 
+document.getElementById('logout').onclick = hacerUnLogout;
+enviar.addEventListener('click', hacerUnPost);
 document.getElementById('theButton').onclick = getAllPhone;
